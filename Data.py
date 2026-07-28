fd413e145b392864ad8e04aa4e27cd08687f757e
janitmreddy-commits <janitmreddy@gmail.com>
2026-07-28 20:55:04 +0530

    Initial commit

---
+++ Data.py
@@ -0,0 +1,12 @@
+age=10
+marks=96.8
+isStudent=True
+name="Janit"
+print ("The age is:", age)
+print ("The marks is:", marks)
+print ("He is astudent?", age)
+print ("The name is:", name)
+print(type(name))
+print(type(age))
+print(type(isStudent))
+print(type(marks))
