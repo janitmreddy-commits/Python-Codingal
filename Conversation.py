2a7968b9a9d754a56185b4ffdf4bfbb45b8edad3
janitmreddy-commits <janitmreddy@gmail.com>
2026-07-31 20:03:24 +0530

    Homework

---
+++ Conversation.py
@@ -0,0 +1,15 @@
+age=10
+grade = 5
+isStudent=True
+name="Janit"
+school="SSRVM"
+print ("The age is:", age)
+print ("The secation is:", grade)
+print ("He is astudent?", age)
+print ("The name is:", name)
+print ("The school:", school)
+print(type(name))
+print(type(age))
+print(type(isStudent))
+print(type(grade))
+print(type(school))
