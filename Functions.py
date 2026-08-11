5766b0c25e7ee434a644d5dedfa734ea4a4bf22e
janitmreddy-commits <janitmreddy@gmail.com>
2026-08-11 20:48:47 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,4 @@
+def greetinges (name):
+    print ("Good moring,",name)
+name = input ("Enter your name:")
+greetinges (name)
---
+++ Marks.py
@@ -0,0 +1,10 @@
+def greetings(marks):
+    if marks > 35:
+
+        print("Congratulations your score is,",marks)
+        
+    else:
+        print ("Work hard")
+marks = int(input("Enter your score"))
+greetings (marks)
+        
