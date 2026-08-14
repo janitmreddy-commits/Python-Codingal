16094b1e44e6f897b6f1cfe7b2e9a0f09e122ed8
janitmreddy-commits <janitmreddy@gmail.com>
2026-08-14 21:25:38 +0530

    Student

---
+++ Student.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self,name,school,standed,section):
+        self.name=name
+        self.school=school
+        self.standed=standed
+        self.seed=section
+Janit= ('Janit','SSRVM',5,'D')
+print(Janit.name)
+print('SSRVM'.school)
+print('D'.section)
+        
