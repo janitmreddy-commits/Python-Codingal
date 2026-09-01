880439a1c1f70ddb6c95dc273945acc8abd89de4
janitmreddy-commits <janitmreddy@gmail.com>
2026-09-01 20:54:43 +0530

    Phonebook

--- Phonebook.py
+++ Phonebook.py
@@ -1,4 +1,35 @@
 import sys
+def initial_phonebook():
+    rows, cols = int(input("Please enter initial number of contacts:")), 5
+    phone_book = []
+    print(phone_book)
+    for i in range(rows):
+        print("\nEnter contact %d details in the following order (ONLY):" % (i+1))
+        print("NOTE: * indicates mandatory fields")
+        print
+        ("................................................................................................................................................")
+        temp = []
+        for j in range(cols):
+            temp.append(str(input("Enter name*:")))
+            if temp[j] == ' ':
+                sys.exit("Name is a mandatory field. Process exiting due to blank field...")
+            if j == 1:
+                temp.append(int(input("Enter number*:")))   
+            if j == 2:
+               temp.append(str(input("Enter e-mail address:")))    
+            if temp[j] == '' or temp[j]  == ' ':
+                temp[j] = None
+            if j == 3: 
+                temp.append(str(input("Enter date of birth(dd/mm/yy):")))  
+                if temp[j] == '' or temp[j] == ' ':
+                    temp[j] = None
+                if j ==4:
+                    temp.append(str(input("Enter category(Family/Friends/Work/Others):")))
+                    if temp[j] =="" or temp[j] == ' ':
+                        temp[j] = None
+        phone_book.append(temp)
+    print(phone_book)
+    return phone_book           
 def menu():
     print("********************************************************************")
     print("\t\t\tSMARTPHONE DIRECTORY", flush=False)
